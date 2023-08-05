4c7c04726e9fc4e5bdc1d0d2a6e46f680f7091b1
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-08-05 12:47:32 +0530

    Time: 2 ms (49.17%), Space: 6 MB (35.20%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[20];
     int solveRec(int n){
         if(n<=1) return 1;
         int sum=0;
@@ -8,7 +9,18 @@ public:
         }
         return sum;
     }
+    int solveMem(int n){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int sum=0;
+        for(int i=1;i<=n;i++){  // think i as a root node
+            sum+=solveMem(i-1)*solveMem(n-i);
+        }
+        return dp[n]=sum;
+    }
     int numTrees(int n) {
-        return solveRec(n);
+        //return solveRec(n);
+        memset(dp,-1,sizeof(dp));
+        return solveMem(n);
     }
 };
