05c2e6077cffa8b82f9f69e664703ca9399f485f
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-08-08 12:06:46 +0530

    Time: 24 ms (99.56%), Space: 6.3 MB (93.68%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[201][201];
+    int dp[202][202];
     int solveRec(int start,int end){
         if(start>=end) return 0;
         int ans=INT_MAX;
@@ -18,9 +18,26 @@ public:
         }
         return dp[start][end]=ans;
     }
+    int solveTab(int n){
+        for(int start=n;start>=1;start--){
+            for(int end=start;end<=n;end++){
+                if(start==end){
+                    continue;
+                }else{
+                    int ans=INT_MAX;
+                    for(int i=start;i<=end;i++){
+                        ans=min(ans,i+max(dp[start][i-1],dp[i+1][end]));
+                    }
+                    dp[start][end]=ans;
+                }
+            }
+        }
+        return dp[1][n];
+    }
     int getMoneyAmount(int n) {
        // return solveRec(1,n);
-       memset(dp,-1,sizeof(dp));
-       return solveMem(1,n);
+       memset(dp,0,sizeof(dp));
+       //return solveMem(1,n);
+       return solveTab(n);
     }
 };
