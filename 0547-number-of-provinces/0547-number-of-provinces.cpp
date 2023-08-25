79907d0e0278fe264baa35ca6749b76d57fe9c0a
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-08-25 23:24:46 +0530

    Time: 22 ms (82.70%), Space: 19.9 MB (5.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
+    // Method 1: using dfs
     void DFS(unordered_map<int,vector<int>> &adj, int u,vector<bool>& visited){
         visited[u]=true;
         for(int &v:adj[u]){
             if(!visited[v]){
                 DFS(adj,v,visited);
+            }
+        }
+    }
+    
+    // Method 2: using bfs
+    void BFS(unordered_map<int,vector<int>> &adj, int u,vector<bool>& visited){
+        queue<int> q;
+        q.push(u);
+        visited[u]=true;
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            
+            for(int &v:adj[curr]){
+                if(!visited[v]){
+                    BFS(adj,v,visited);
+                }
             }
         }
     }
@@ -23,7 +41,8 @@ public:
         int cnt=0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                DFS(adj,i,visited);
+                //DFS(adj,i,visited);
+                BFS(adj,i,visited);
                 cnt++;
             }
         }
