a5dcfd929a34fd8bbc3f1b711118aebd21ff1194
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-08-25 23:10:58 +0530

    Time: 24 ms (73.49%), Space: 17.2 MB (8.78%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void DFS(unordered_map<int,vector<int>> &adj, int u,vector<bool>& visited){
+        visited[u]=true;
+        for(int &v:adj[u]){
+            if(!visited[v]){
+                DFS(adj,v,visited);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<bool> visited(n,false);
+        unordered_map<int,vector<int>> adj;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                DFS(adj,i,visited);
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
