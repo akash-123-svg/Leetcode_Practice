38f6bd1e0341964eeaa17cdefa715b8128e788a9
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-08-25 13:03:33 +0530

    Time: 22 ms (35.47%), Space: 15.3 MB (38.03%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -23,6 +23,7 @@ public:
         }
         return res;
     }
+    // Method 1:- using 3D-DP, Time:- O(l*m*n)
     bool solveMem(int i,int j,int k,string &s1,string &s2,string &s3){
         int l=s1.length();
         int m=s2.length();
@@ -46,11 +47,37 @@ public:
         }
         return dp[i][j][k]=res;
     }
+    // Method 2: using 2D-DP, Time:- O(l*n)
+    int dp1[101][101];
+    bool solveMem1(int i,int j,string &s1,string &s2,string &s3){
+        int l=s1.length();
+        int m=s2.length();
+        int n=s3.length();
+        if(i==l && j==m && i+j==n){  // k is replace with i+j thats it..
+            return true;
+        }
+        if(i+j>=n){
+            return false;
+        }
+        if(dp1[i][j]!=-1) return dp1[i][j];
+        bool res=false;
+        if(s1[i]==s3[i+j]){
+            res=solveMem1(i+1,j,s1,s2,s3);
+        }
+        if(res==true){
+            return dp1[i][j]=true;
+        }
+        if(s2[j]==s3[i+j]){
+            res=solveMem1(i,j+1,s1,s2,s3);
+        }
+        return dp1[i][j]=res;
+    }
     bool isInterleave(string s1, string s2, string s3) {
         int l=s1.length();
         int m=s2.length();
         int n=s3.length();
-        memset(dp,-1,sizeof(dp));
-        return solveMem(0,0,0,s1,s2,s3);
+        //memset(dp,-1,sizeof(dp));
+        memset(dp1,-1,sizeof(dp1));
+        return solveMem1(0,0,s1,s2,s3);
     }
 };
