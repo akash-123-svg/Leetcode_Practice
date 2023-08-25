eb9e52d76a737b9295aad6c966cb724f17f038cd
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-08-25 23:57:48 +0530

    Time: 17 ms (88.51%), Space: 14 MB (36.91%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool Toposort(unordered_map<int,vector<int>> &adj,int n,vector<int> &indegree){
+        queue<int> q;
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                cnt++;
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop(); 
+            for(int &v:adj[curr]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    cnt++;
+                    q.push(v);
+                }
+            }
+        }
+        if(cnt==n){  // I was able to finish all the nodes (courses)
+            return true;
+        }
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        unordered_map<int,vector<int>> adj;
+        vector<int> indegree(n,false);
+        for(auto &vec : prerequisites){
+            int a=vec[0];
+            int b=vec[1];
+            
+           // b--->a
+           adj[b].push_back(a);
+           indegree[a]++;
+        }
+        return Toposort(adj,n,indegree);
+    }
+};
