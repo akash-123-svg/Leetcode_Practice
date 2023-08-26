28455b4494237c019a3a7d22e7cbd7f7e4e2ab30
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-08-26 19:20:12 +0530

    Time: 22 ms (52.93%), Space: 14 MB (41.20%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> TopoSort(unordered_map<int,vector<int>> &adj,int n,vector<int> &indegree){
+        queue<int> q;
+        int cnt=0;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                ans.push_back(i);
+                cnt++;
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            
+            for(int &v:adj[u]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    ans.push_back(v);
+                    cnt++;
+                    q.push(v);
+                }
+            }
+        }
+        if(cnt==n){
+            return ans;
+        }
+        return {};
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        unordered_map<int,vector<int>> adj;
+        vector<int> indegree(n,0);
+        
+        for(auto &vec:prerequisites){
+            int a=vec[0];
+            int b=vec[1];
+            
+            // b--->a
+            adj[b].push_back(a);
+            
+            indegree[a]++;
+        }
+        return TopoSort(adj,n,indegree);
+    }
+};
