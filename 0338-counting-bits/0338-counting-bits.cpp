b682a5f8dca91f7b990925a3b9457cefafabefce
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-09-01 10:43:12 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        // Method 1:- using built-in function 
+        // Method 1:- using built-in function, Time:- O(nlogn)
         // vector<int> ans(n+1);
         // for(int i=1;i<=n;i++){
         //     ans[i]=__builtin_popcount(i);
         // }
         // return ans;
         
-        // Method 2: without using any built-in function 
+        // Method 2: without using any built-in function, Time:- O(n)
          vector<int> ans(n+1);
          ans[0]=0;
          for(int i=1;i<=n;i++){
