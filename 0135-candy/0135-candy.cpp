1e0f0f77395717f50ca7df6ffd8932a0aa26f5e2
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-09-13 18:53:15 +0530

    Time: 19 ms (45.37%), Space: 18.5 MB (10.64%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+      int n=ratings.size();
+      vector<int> right(n,1);
+      vector<int> left(n,1);
+      for(int i=1;i<n;i++){
+          if(ratings[i]>ratings[i-1]){
+              left[i]=left[i-1]+1;
+          }
+      }
+      for(int i=n-2;i>=0;i--){
+          if(ratings[i]>ratings[i+1]){
+              right[i]=right[i+1]+1;
+          }
+      }
+      int candies=0;
+      for(int i=0;i<n;i++){
+          int maxx=max(left[i],right[i]);
+          candies+=maxx;
+      }  
+      return candies; 
+    }
+};
