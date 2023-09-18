fe16eee511d7e5f5dedd049f95f3682c031d1593
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-09-18 21:03:38 +0530

    Time: 13 ms (66.69%), Space: 12.2 MB (69.50%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -42,9 +42,32 @@ public:
         }
         return dp[Ktrans][index][buy]=max_profit;
     }
+    int solveTab(int k,vector<int>& prices){
+        int n=prices.size();
+        memset(dp,0,sizeof(dp));
+        for(int Ktrans=1;Ktrans<=k;Ktrans++){
+            for(int index=n-1;index>=0;index--){
+                for(int buy=0;buy<=1;buy++){
+                    int max_profit=0;
+                    if(buy==1){
+                        int buykaro=(-prices[index])+dp[Ktrans][index+1][0];
+                        int skipkaro=0+dp[Ktrans][index+1][1];
+                        max_profit=max(buykaro,skipkaro);
+                    }else{
+                        int sellkaro=(prices[index])+dp[Ktrans-1][index+1][1];
+                        int skipkaro=0+dp[Ktrans][index+1][0];
+                        max_profit=max(sellkaro,skipkaro);
+                    }
+                    dp[Ktrans][index][buy]=max_profit;
+                }
+            }
+        }
+        return dp[k][0][1];
+    }
     int maxProfit(int k, vector<int>& prices) {
        //return solveRec(k,prices,0,1); 
-       memset(dp,-1,sizeof(dp));
-       return solveMem(k,prices,0,1);
+       // memset(dp,-1,sizeof(dp));
+       // return solveMem(k,prices,0,1);
+        return solveTab(k,prices);
     }
 };
