4b765d354951e27586eb245740b76012dbd18380
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-09-24 08:35:41 +0530

    Time: 58 ms (99.15%), Space: 61.5 MB (46.26%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -11,8 +11,26 @@ public:
         }
         return cnt;
     }
+    
+    // Opti app:- Time :- O(n), space:- O(1);
+    int solve2(vector<int>& nums){
+       int slow=nums[0];
+       int fast=nums[0];
+       do{
+           slow=nums[slow];
+           fast=nums[nums[fast]];
+       }
+       while(slow!=fast); 
+          slow=nums[0];
+       while(slow!=fast){
+           slow=nums[slow];
+           fast=nums[fast];
+       }
+       return slow;
+    }
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return solve1(nums);
+        // sort(nums.begin(),nums.end());
+        // return solve1(nums);
+        return solve2(nums);
     }
 };
