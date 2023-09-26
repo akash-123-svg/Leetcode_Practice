9831c0e207e733de33ef76e698658bf9ef5a6e0e
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-09-26 12:57:05 +0530

    Time: 54 ms (43.40%), Space: 29 MB (6.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -26,9 +26,27 @@ public:
         }
         return dp[i][j]=ans;
     }
+    int solveTab(string &text1,string &text2){
+        int n=text1.length();
+        int m=text2.length();
+        vector<vector<int>>dp1(n+1,vector<int>(m+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans=0;
+                if(text1[i]==text2[j]){
+                    ans=1+dp1[i+1][j+1];
+                }else{
+                    ans=max(dp1[i][j+1],dp1[i+1][j]);
+                }
+                dp1[i][j]=ans;
+            }
+        }
+        return dp1[0][0];
+    }
     int longestCommonSubsequence(string text1, string text2) {
         //return solveRec(text1,text2,0,0);
-        memset(dp,-1,sizeof(dp));
-        return solveMem(text1,text2,0,0);
+        // memset(dp,-1,sizeof(dp));
+        // return solveMem(text1,text2,0,0);
+        return solveTab(text1,text2);
     }
 };
