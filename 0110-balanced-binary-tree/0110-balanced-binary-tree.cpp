b630a95acb20596311f367ef459bdd640d0072df
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-10-17 13:08:33 +0530

    Time: 11 ms (45.30%), Space: 22.4 MB (5.68%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -20,18 +20,41 @@ public:
         int result=max(left_subtree,right_subtree)+1;
         return result;
     }
-    bool isBalanced(TreeNode* root) {
+    pair<bool,int> isBalancedfast(TreeNode* root){
         if(root==NULL){
-            return true;
+            pair<bool,int> p=make_pair(true,0);
+            return p;
         }
-        bool op1=isBalanced(root->left);
-        bool op2=isBalanced(root->right);
-        bool op3=abs(height(root->left)-height(root->right))<=1;
+        pair<bool,int> left_subtree=isBalancedfast(root->left);
+        pair<bool,int> right_subtree=isBalancedfast(root->right);
+        
+        bool op1=left_subtree.first; // left
+        bool op2=right_subtree.first; // right
+        bool op3=abs((left_subtree.second)-(right_subtree.second))<=1;
+        
+        pair<bool,int> ans;
+        ans.second=max(left_subtree.second,right_subtree.second)+1;
         
         if(op1 && op2 && op3){
-            return true;
+            ans.first=true;
         }else{
-            return false;
+            ans.first=false;
         }
+        return ans;
+    }
+    bool isBalanced(TreeNode* root) {
+//         if(root==NULL){
+//             return true;
+//         }
+//         bool op1=isBalanced(root->left);
+//         bool op2=isBalanced(root->right);
+//         bool op3=abs(height(root->left)-height(root->right))<=1;
+        
+//         if(op1 && op2 && op3){
+//             return true;
+//         }else{
+//             return false;
+//         }
+          return isBalancedfast(root).first;
     }
 };
