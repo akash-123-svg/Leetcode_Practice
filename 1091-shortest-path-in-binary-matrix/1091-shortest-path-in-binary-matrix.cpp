507a35d0b213a6778cbbf5a00167c2bd883fb9e4
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-10-19 14:40:13 +0530

    Time: 79 ms (31.85%), Space: 20.2 MB (79.56%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,48 +3,40 @@ public:
     typedef pair<int,pair<int,int>> P;
     vector<vector<int>> directions={{1,1},{-1,1},{1,-1},{-1,-1},{0,1},{1,0},{0,-1},{-1,0}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
         if(n==0 || m==0 || grid[0][0]!=0){
             return -1;
         }
         auto isSafe=[&](int x,int y){
-           if(x>=0 && x<m && y>=0 && y<n){
-               return true;
-           }
-            return false;
+            return x>=0 && x<n && y>=0 && y<m;
         };
-        vector<vector<int>> res(m,vector<int>(n,INT_MAX));
-        
-        // apply dijkstra algorithm here
         priority_queue<P,vector<P>,greater<P>> pq;
-        
+        vector<vector<int>> ans(n,vector<int>(m,INT_MAX));
+        ans[0][0]=0;
         pq.push({0,{0,0}});
-        res[0][0]=0;
         
         while(!pq.empty()){
             int d=pq.top().first;
-            pair<int,int> node=pq.top().second;
-            int new_x=node.first;
-            int new_y=node.second;
+            pair<int,int>cord=pq.top().second;
+            int newx=cord.first;
+            int newy=cord.second;
             pq.pop();
             
-            for(auto dir : directions){
-                int x_=new_x+dir[0];
-                int y_=new_y+dir[1];
-                
-                int dist=1;
-                
-                if(isSafe(x_,y_) && grid[x_][y_]==0 && d+dist < res[x_][y_]){
-                    pq.push({d+dist,{x_,y_}});
-                    res[x_][y_]=d+dist; 
+            for(auto &dir : directions){
+                int x_=newx+dir[0];
+                int y_=newy+dir[1];
+                int wt=1;
+                if(isSafe(x_,y_) && grid[x_][y_]==0 && d+wt < ans[x_][y_]){
+                    ans[x_][y_]=d+wt;
+                    pq.push({d+wt,{x_,y_}});
                 }
             }
         }
-        if(res[m-1][n-1]==INT_MAX){
+        if(ans[n-1][m-1]==INT_MAX){
             return -1;
         }else{
-            return res[m-1][n-1]+1;
+            return ans[n-1][m-1]+1;
         }
     }
 };
