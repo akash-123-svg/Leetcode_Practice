a9b6d9a444df922f1c69fd56f3f8102cb925873a
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-10-19 12:19:36 +0530

    Time: 23 ms (23.76%), Space: 14.2 MB (48.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool Toposort(unordered_map<int,vector<int>> &adj,int n,vector<int> &indegree){
+    bool Toposort(int n,unordered_map<int,vector<int>>&adj,vector<int>&indegree){
         queue<int> q;
         int cnt=0;
         for(int i=0;i<n;i++){
@@ -11,8 +11,8 @@ public:
         }
         while(!q.empty()){
             int curr=q.front();
-            q.pop(); 
-            for(int &v:adj[curr]){
+            q.pop();
+            for(auto &v:adj[curr]){
                 indegree[v]--;
                 if(indegree[v]==0){
                     cnt++;
@@ -20,23 +20,18 @@ public:
                 }
             }
         }
-        if(cnt==n){  // I was able to finish all the nodes (courses)
-            return true;
-        }
-        return false; // Means there was a cycle and I couldn't complete all courses.
+        return cnt==n;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
-        unordered_map<int,vector<int>> adj;
-        vector<int> indegree(n,false);
-        for(auto &vec : prerequisites){
-            int a=vec[0];
-            int b=vec[1];
-            
-           // b--->a
-           adj[b].push_back(a);
-           indegree[a]++;
+        unordered_map<int,vector<int>>adj;
+        vector<int> indegree(n,0);
+        for(auto &vec: prerequisites){
+            int u=vec[0];
+            int v=vec[1];
+            adj[v].push_back(u);
+            indegree[u]++;
         }
-        return Toposort(adj,n,indegree);
+        return Toposort(n,adj,indegree);
     }
 };
