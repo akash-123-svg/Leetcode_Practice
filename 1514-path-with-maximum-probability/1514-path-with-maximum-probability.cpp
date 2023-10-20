975dd11836ce8efea8ae4008742b129eb9272548
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-10-20 12:23:54 +0530

    Time: 145 ms (74.42%), Space: 65.3 MB (74.42%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node){
-        vector<pair<int,double>>adj[n];
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<pair<int,double>> adj[n];
         for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],succProb[i]});
-            adj[edges[i][1]].push_back({edges[i][0],succProb[i]});
+            int a=edges[i][0];
+            int b=edges[i][1];
+            double c=succProb[i];
+            adj[a].push_back({b,c});
+            adj[b].push_back({a,c});
         }
-        priority_queue<pair<double,int>> pq;
-        vector<double> res(n,INT_MIN);
-        res[start_node]=1.0;
+        priority_queue<pair<double,int>>pq;
+        vector<double>ans(n,INT_MIN);
+        ans[start_node]=1.0;
         pq.push({1.0,start_node});
         
         while(!pq.empty()){
-            auto it=pq.top();
+            pair<double,int> p=pq.top();
             pq.pop();
-            double dis=it.first;
-            int node=it.second;
-            for(auto &vec: adj[node]){
-               int adjNode=vec.first;
-               double edgeWeight=vec.second;
-               if(edgeWeight*dis > res[adjNode]){
-                   res[adjNode]=edgeWeight*dis;
-                   pq.push({res[adjNode],adjNode});
-               }
+            double d=p.first;
+            int node=p.second;
+            
+            for(auto &v: adj[node]){
+                int adjNode=v.first;
+                double wt=v.second;
+                if(d*wt > ans[adjNode]){
+                    ans[adjNode]=d*wt;
+                    pq.push({ans[adjNode],adjNode});
+                }
             }
         }
-        if(res[end_node]==INT_MIN){
+        if(ans[end_node]==INT_MIN){
             return 0.00000;
         }else{
-            return res[end_node];
+            return ans[end_node];
         }
     }
 };
