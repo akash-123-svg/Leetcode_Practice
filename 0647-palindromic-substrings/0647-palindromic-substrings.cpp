c853b8e6438687de1b4bf3bd319895f1085df289
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-10-27 11:50:48 +0530

    Time: 36 ms (21.97%), Space: 11.1 MB (57.34%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+    int dp[1001][1001];
     bool solve(string &s,int i,int j){
         if(i>=j){
-            return true;
+            return 1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         if(s[i]==s[j]){
-            return solve(s,i+1,j-1);
+           return dp[i][j]=solve(s,i+1,j-1);
         }
-        return false;
+        return dp[i][j]=0;
     }
     int countSubstrings(string s) {
         int n=s.length();
         int cnt=0;
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(solve(s,i,j)==true){
