2f659160037cd7d290cac0aef6b67b26a2f99a12
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-10-27 12:07:13 +0530

    Time: 66 ms (81.61%), Space: 11 MB (79.32%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -28,10 +28,28 @@ public:
         }
         return dp[i][j]=ans;
     }
+    int solveTab(string &s,string &t){
+        memset(dp,0,sizeof(dp));
+        int n=s.length();
+        int m=t.length();
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans=0;
+                if(s[i]==t[j]){
+                    ans=1+dp[i+1][j+1];
+                }else{
+                    ans=max(dp[i+1][j],dp[i][j+1]);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[0][0];
+    }
     int longestPalindromeSubseq(string s) {
         string t=s;
         reverse(t.begin(),t.end());
-        memset(dp,-1,sizeof(dp));
-        return solveMem(s,t,0,0);
+        // memset(dp,-1,sizeof(dp));
+        // return solveMem(s,t,0,0);
+        return solveTab(s,t);
     }
 };
