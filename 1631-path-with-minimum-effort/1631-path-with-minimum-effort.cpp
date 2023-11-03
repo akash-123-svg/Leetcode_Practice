ab8bb5e296f970a8bc11a24414685bdeb68471b0
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-11-03 12:21:36 +0530

    Time: 134 ms (46.10%), Space: 20.4 MB (46.72%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> P;
-    vector<vector<int>> directions={{-1,0},{0,-1},{0,1},{1,0}};
+    typedef pair<int,pair<int,int>>P;
+    vector<vector<int>> dirs={{-1,0},{1,0},{0,-1},{0,1}};
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n=heights.size();
         int m=heights[0].size();
-        priority_queue<P,vector<P>,greater<P>> pq;
+        
         vector<vector<int>>res(n+1,vector<int>(m+1,INT_MAX));
+        priority_queue<P,vector<P>,greater<P>> pq;
+        
         res[0][0]=0;
         pq.push({0,{0,0}});
         
-        auto isSafe = [&](int x,int y){
-            return x>=0 && x<n && y>=0 && y<m;
+        auto isSafe=[&](int x,int y){
+            return (x>=0 && x<n && y>=0 && y<m);
         };
+        
         while(!pq.empty()){
             int diff=pq.top().first;
-            pair<int,int> coord=pq.top().second;
-            int x=coord.first;
-            int y=coord.second;
+            pair<int,int> coords=pq.top().second;
+            int x=coords.first;
+            int y=coords.second;
             pq.pop();
             
-            for(auto &dir : directions){
+            for(auto &dir: dirs){
                 int newx=x+dir[0];
                 int newy=y+dir[1];
                 
                 if(isSafe(newx,newy)){
-                    int absDiff=abs(heights[x][y]-heights[newx][newy]);
-                    int maxDiff=max(diff,absDiff);
-                    if(maxDiff < res[newx][newy]){
-                        res[newx][newy]=maxDiff;
-                        pq.push({maxDiff,{newx,newy}});
-                    }
-                }
+                    
+                int absDiff=abs(heights[x][y]-heights[newx][newy]);
+                int maxxDiff=max(diff,absDiff);
+                
+                if(res[newx][newy] > maxxDiff){
+                    res[newx][newy]=maxxDiff;
+                    pq.push({maxxDiff,{newx,newy}});
+                 }
+              }
             }
         }
         return res[n-1][m-1];
