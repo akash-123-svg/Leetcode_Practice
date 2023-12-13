fd55a065239a479ffa6565b23bd387630d509a14
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-12-13 14:25:04 +0530

    Time: 2 ms (44.17%), Space: 6.4 MB (85.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int dp[101][101];
-    int solveRec(int col,int row,int m,int n){
+    int solve(int m,int n,int col,int row){
         if(col>=m || row>=n){
             return 0;
-        }else if(col==m-1 && row==n-1){
+        }
+        if(col==m-1 && row==n-1){
             return 1;
         }else{
-            int x=solveRec(col+1,row,m,n);
-            int y=solveRec(col,row+1,m,n);
+            int x=solve(m,n,col+1,row);
+            int y=solve(m,n,col,row+1);
             return (x+y);
         }
     }
-    int solveMem(int col,int row,int m,int n){
+    int solveMem(int m,int n,int col,int row){
         if(col>=m || row>=n){
             return 0;
-        }if(col==m-1 && row==n-1){
+        }
+        if(dp[col][row]!=-1){
+           return dp[col][row];
+        }
+        if(col==m-1 && row==n-1){
             return 1;
-        }if(dp[col][row]!=-1) return dp[col][row];
-        else{
-            int x=solveMem(col+1,row,m,n);
-            int y=solveMem(col,row+1,m,n);
+        }else{
+            int x=solveMem(m,n,col+1,row);
+            int y=solveMem(m,n,col,row+1);
             return dp[col][row]=(x+y);
         }
     }
     int uniquePaths(int m, int n) {
-       // return solveRec(0,0,m,n);
-        memset(dp,-1,sizeof(dp));
-        return solveMem(0,0,m,n);
+       memset(dp,-1,sizeof(dp));
+       return solveMem(m,n,0,0); 
     }
 };
