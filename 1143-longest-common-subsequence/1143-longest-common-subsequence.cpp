73f4b39a328da80e7e9d2c87119ad57919c136ce
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2023-12-13 14:37:54 +0530

    Time: 38 ms (74.92%), Space: 11.3 MB (75.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,52 +1,35 @@
 class Solution {
 public:
     int dp[1001][1001];
-    int solveRec(string &text1,string &text2,int i,int j){
-        if(i==text1.length() || j==text2.length()){
+    int solve(string &s, string &t,int i,int j){
+        if(i==s.length() || j==t.length()){
             return 0;
         }
         int ans=0;
-        if(text1[i]==text2[j]){
-            ans=1+solveRec(text1,text2,i+1,j+1);
+        if(s[i]==t[j]){
+            ans=1+solve(s,t,i+1,j+1);
         }else{
-            ans=max(solveRec(text1,text2,i+1,j),solveRec(text1,text2,i,j+1));
+            ans=max(solve(s,t,i+1,j),solve(s,t,i,j+1));
         }
         return ans;
     }
-    int solveMem(string &text1,string &text2,int i,int j){
-        if(i==text1.length() || j==text2.length()){
+    int solveMem(string &s, string &t,int i,int j){
+        if(i==s.length() || j==t.length()){
             return 0;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
         int ans=0;
-        if(text1[i]==text2[j]){
-            ans=1+solveMem(text1,text2,i+1,j+1);
+        if(s[i]==t[j]){
+            ans=1+solveMem(s,t,i+1,j+1);
         }else{
-            ans=max(solveMem(text1,text2,i+1,j),solveMem(text1,text2,i,j+1));
+            ans=max(solveMem(s,t,i+1,j),solveMem(s,t,i,j+1));
         }
         return dp[i][j]=ans;
     }
-    int solveTab(string &text1,string &text2){
-        int n=text1.length();
-        int m=text2.length();
-        vector<vector<int>>dp1(n+1,vector<int>(m+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int j=m-1;j>=0;j--){
-                int ans=0;
-                if(text1[i]==text2[j]){
-                    ans=1+dp1[i+1][j+1];
-                }else{
-                    ans=max(dp1[i][j+1],dp1[i+1][j]);
-                }
-                dp1[i][j]=ans;
-            }
-        }
-        return dp1[0][0];
-    }
     int longestCommonSubsequence(string text1, string text2) {
-        //return solveRec(text1,text2,0,0);
-        // memset(dp,-1,sizeof(dp));
-        // return solveMem(text1,text2,0,0);
-        return solveTab(text1,text2);
+        memset(dp,-1,sizeof(dp));
+        return solveMem(text1,text2,0,0);
     }
 };
