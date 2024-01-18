0a4561dcbcefa8d5dc93ead8d85542be20f81e7b
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-01-18 12:32:48 +0530

    Time: 2 ms (45.07%), Space: 6.2 MB (88.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,8 +10,16 @@ public:
         if(dp[n]!=-1) return dp[n];
         return dp[n]=solveMem(n-1)+solveMem(n-2);
     }
+    int solveTab(int n){
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     int climbStairs(int n) {
-       memset(dp,-1,sizeof(dp));
-       return solveMem(n);
+       memset(dp,0,sizeof(dp));
+       return solveTab(n);
     }
 };
