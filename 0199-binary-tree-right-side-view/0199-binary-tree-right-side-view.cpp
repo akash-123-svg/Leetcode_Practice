8d648a1667ca99fad5a462deadb159915ade902a
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-03-30 07:03:41 +0530

    Time: 0 ms (100.00%), Space: 14.3 MB (19.10%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,16 +11,9 @@
  */
 class Solution {
 public:
-    void Rview(TreeNode* root, int level, vector<int>&ans){
-        if(!root) return;
-        if(level==ans.size()) ans.push_back(root->val);
-        Rview(root->right,level+1,ans);
-        Rview(root->left,level+1,ans);
-    }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
         if(!root) return ans;
-        //Rview(root, 0,ans);
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
