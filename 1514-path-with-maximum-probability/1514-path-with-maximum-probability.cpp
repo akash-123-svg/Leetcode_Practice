a62e015a200ddb4a0650e15c0a1c89c4f1b8c3a7
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-04-15 22:43:34 +0530

    Time: 123 ms (84.78%), Space: 68.6 MB (84.25%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
         vector<pair<int,double>> adj[n];
+        vector<double> ans(n,INT_MIN);
         for(int i=0;i<edges.size();i++){
             int a=edges[i][0];
             int b=edges[i][1];
@@ -10,27 +11,27 @@ public:
             adj[b].push_back({a,c});
         }
         priority_queue<pair<double,int>> pq;
-        vector<double> ans(n,INT_MIN);
         ans[start_node]=1.0;
         pq.push({1.0,start_node});
         
         while(!pq.empty()){
             pair<double,int> p=pq.top();
-            pq.pop();
             double d=p.first;
             int node=p.second;
+            pq.pop();
             
-            for(auto &v: adj[node]){
-                int adjNode=v.first;
-                double dis=v.second;
-                if(d*dis > ans[adjNode]){
-                    ans[adjNode]=d*dis;
+            for(auto & vec: adj[node]){
+                int adjNode=vec.first;
+                double wt=vec.second;
+                
+                if(d*wt > ans[adjNode]){
+                    ans[adjNode]=d*wt;
                     pq.push({ans[adjNode],adjNode});
                 }
             }
         }
         if(ans[end_node]==INT_MIN){
-            return 0.00000; 
+            return 0.00000;
         }else{
             return ans[end_node];
         }
