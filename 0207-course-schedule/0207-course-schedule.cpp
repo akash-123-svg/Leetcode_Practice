a02b0ed33e716446235533bf9f36024f7c466f6b
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-06-12 13:44:10 +0530

    Time: 15 ms (70.32%), Space: 17.6 MB (43.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    bool Topo(int n, vector<vector<int>>& prerequisites, vector<int>& indegree, unordered_map<int,vector<int>>& adj){
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        unordered_map<int,vector<int>> adj;
+        vector<int> indegree(n,0);
+        for(auto &vec:  prerequisites){
+            int u=vec[0];
+            int v=vec[1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
         queue<int> q;
         int cnt=0;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
-                cnt++;
                 q.push(i);
+                cnt++;
             }
         }
         while(!q.empty()){
-            int f=q.front();
+            int sz=q.front();
             q.pop();
             
-            for(auto &x: adj[f]){
-                indegree[x]--;
-                if(indegree[x]==0){
+            for(auto &vec: adj[sz]){
+                indegree[vec]--;
+                if(indegree[vec]==0){
+                    q.push(vec);
                     cnt++;
-                    q.push(x);
                 }
             }
         }
-        if(cnt==n){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        vector<int> indegree(n,0);
-        unordered_map<int,vector<int>> adj;
-        for(auto &vec: prerequisites){
-            int a=vec[0];
-            int b=vec[1];
-            adj[b].push_back(a);
-            indegree[a]++;
-        }
-        return Topo(n, prerequisites, indegree, adj);
+        if(cnt==n) return true;
+        else return false;
     }
 };
