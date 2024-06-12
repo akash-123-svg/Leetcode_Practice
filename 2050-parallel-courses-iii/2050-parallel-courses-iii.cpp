0f37870578007fe2226531a30b7b21c9511cd67a
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-06-12 12:42:54 +0530

    Time: 327 ms (36.62%), Space: 147.8 MB (42.89%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
-        unordered_map<int,vector<int>> adj;
-        vector<int> indegree(n,0);
-        for(int i=0;i<relations.size();i++){
-            int u=relations[i][0]-1;
-            int v=relations[i][1]-1;
-            adj[u].push_back(v);
-            indegree[v]++;
-        }
-        queue<int> q;
-        vector<int> max_time(n,0);
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-                max_time[i]=time[i];
-            }
-        }
-        while(!q.empty()){
-            int sz=q.front();
-            q.pop();
-            
-            for(auto &vec: adj[sz]){
-                max_time[vec]=max(max_time[vec],max_time[sz]+time[vec]);
-                indegree[vec]--;
-                if(indegree[vec]==0){
-                    q.push(vec);
-                }
-            }
-        }
-        return *max_element(max_time.begin(),max_time.end());
+         unordered_map<int,vector<int>> adj;
+         vector<int> indegree(n,0);
+         for(int i=0;i<relations.size();i++){
+             int u=relations[i][0]-1;
+             int v=relations[i][1]-1;
+             adj[u].push_back(v);
+             indegree[v]++;
+         }
+         vector<int> min_months(n,0);
+         queue<int> q;
+         for(int i=0;i<n;i++){
+             if(indegree[i]==0){
+                 q.push(i);
+                 min_months[i]=time[i];
+             }
+         }
+         while(!q.empty()){
+             int sz=q.front();
+             q.pop();
+             
+             for(auto &vec: adj[sz]){
+                 min_months[vec]=max(min_months[vec],min_months[sz]+time[vec]);
+                 indegree[vec]--;
+                 if(indegree[vec]==0){
+                     q.push(vec);
+                 }
+             }
+         }
+         return *max_element(min_months.begin(),min_months.end());
     }
 };
