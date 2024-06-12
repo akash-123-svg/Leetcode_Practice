a36b960bb7d6f1ccf5e1d25407fc067dc8e3d335
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-06-12 15:28:40 +0530

    Time: 15 ms (67.78%), Space: 17.5 MB (44.97%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    vector<int>Topo(int n, vector<vector<int>>& prerequisites, vector<int>& indegree, unordered_map<int,vector<int>>& adj){
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        unordered_map<int,vector<int>>adj;
+        vector<int> indegree(n,0);
+        for(auto & vec: prerequisites){
+            int u=vec[0];
+            int v=vec[1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
         queue<int> q;
         vector<int> ans;
         int cnt=0;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
-                cnt++;
                 q.push(i);
                 ans.push_back(i);
+                cnt++;
             }
         }
         while(!q.empty()){
-            int f=q.front();
+            int sz=q.front();
             q.pop();
             
-            for(auto &x: adj[f]){
-                indegree[x]--;
-                if(indegree[x]==0){
+            for(auto &vec: adj[sz]){
+                indegree[vec]--;
+                
+                if(indegree[vec]==0){
+                    q.push(vec);
+                    ans.push_back(vec);
                     cnt++;
-                    q.push(x);
-                    ans.push_back(x);
                 }
             }
         }
@@ -29,18 +39,5 @@ public:
         }else{
             return {};
         }
-    }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n=numCourses;
-        vector<int> ans;
-        vector<int> indegree(n,0);
-        unordered_map<int,vector<int>>adj;
-        for(auto &vec: prerequisites){
-            int a=vec[0];
-            int b=vec[1];
-            adj[b].push_back(a);
-            indegree[a]++;
-        }
-        return Topo(n,prerequisites,indegree, adj);
     }
 };
