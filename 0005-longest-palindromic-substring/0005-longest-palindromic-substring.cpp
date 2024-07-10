7829f9f00d5279e79b6e823d3572845a26dd997c
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-07-10 12:51:54 +0530

    Time: 841 ms (14.96%), Space: 8.3 MB (75.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    bool solve(string &s,int i,int j){
-       if(i>=j){
-           return 1;
-       }
-       if(dp[i][j]!=-1){
-           return dp[i][j];
-       }
-       if(s[i]==s[j]){
-           return dp[i][j]=solve(s,i+1,j-1);
-       }
-       return dp[i][j]=0;
+    int solve(string &s, int i,int j){
+        if(i>=j) return 1;
+        if(s[i]==s[j]){
+            return solve(s,i+1,j-1);
+        }
+        return 0;
     }
     string longestPalindrome(string s) {
         int n=s.length();
-        int maxlen=INT_MIN;
-        int startPoint=0;
-        memset(dp,-1,sizeof(dp));
+        int max_len=INT_MIN;
+        int startpoint=0;
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(solve(s,i,j)==true){
-                    if(j-i+1 > maxlen){
-                        maxlen=j-i+1;
-                        startPoint=i;
+                    if(j-i+1>max_len){
+                        max_len=j-i+1;
+                        startpoint=i;
                     }
                 }
             }
         }
-        return s.substr(startPoint,maxlen);
+        return s.substr(startpoint,max_len);
     }
 };
