f124984a866c30198861462140a9973ad45ffef1
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-11-02 12:34:47 +0530

    Time: 0 ms (100.00%), Space: 8 MB (96.30%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]==' '){
+                if(s[i-1]!=s[i+1]){
+                    return false;
+                }
+            }
+        }
+        if(s[0]==s[n-1]){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
