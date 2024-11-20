e315f033072990818c9f14d38e2bb34a808bc21f
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2024-11-20 13:13:12 +0530

    Time: 5 ms (62.11%), Space: 19.6 MB (37.35%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
         int n=numCourses;
-        unordered_map<int,vector<int>>adj;
+        unordered_map<int,vector<int>> adj;
         vector<int> indegree(n,0);
-        for(auto &vec: prerequisites){
+        for(auto &vec: pre){
             int u=vec[0];
             int v=vec[1];
             adj[v].push_back(u);
             indegree[u]++;
         }
-        queue<int> q;
         int cnt=0;
+        queue<int> q;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
                 q.push(i);
